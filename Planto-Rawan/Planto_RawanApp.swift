6ec9e7ffb517860608b0f65788f7dfa97cd05555
Rawanalmutahar <Rawanalmutahar@hotmail.com>
2024-10-20 09:05:46 +0300

    Initial Commit

---
+++ Planto-Rawan/Planto_RawanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planto_RawanApp.swift
+//  Planto-Rawan
+//
+//  Created by Rawan on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Planto_RawanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
